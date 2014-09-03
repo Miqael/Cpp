57c34db4c84ce8ed1756aacdd6a8f3f27465da96
Alaverdyan Miqael <Miqael@Alaverdyans-MacBook-Pro.local>
2014-09-04 00:30:54 +0400

    Initial Commit

---
+++ AlaverdyanCpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AlaverdyanCpp
+//
+//  Created by Alaverdyan Miqael on 9/4/14.
+//  Copyright (c) 2014 Alaverdyan Miqael. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
